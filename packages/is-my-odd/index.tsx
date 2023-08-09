9d42529a909a41c0ef9aa7b3676b0851ba71ebb0
Derek Lindahl <dlindahl@activision.com>
2023-08-09 16:02:10 -0700

    Attempt to inline auth0-react

--- packages/is-my-odd/index.tsx
+++ packages/is-my-odd/index.tsx
@@ -2,6 +2,8 @@ import { isMyEven } from '@example/is-my-even';
 import React from 'react'
 import { roles } from 'aria-query'
 import * as dummy from 'aria-query/lib/etc/roles/abstract/commandRole'
+import { initialAuthState as initialAuthStateDist } from '@auth0/auth0-react/dist/auth-state'
+// import { initialAuthState as initialAuthStateSrc } from '@auth0/auth0-react/src/auth-state'
 
 export const isMyOdd = (x: number) => !isMyEven(x);
 
@@ -10,6 +12,8 @@ export const someOtherValue = 123
 export function IsMyOdd(): JSX.Element {
   console.info({ roles })
   console.info({ dummy })
+  console.info({ initialAuthStateDist })
+  // console.info({ initialAuthStateSrc })
 
   return <div>is-my-odd?</div>
 }
